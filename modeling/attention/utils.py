88cb1a4c25261e2772a86d6d91e804ce2c805374
alexaisophie <131282925+alexaisophie@users.noreply.github.com>
2025-06-12 16:33:22 +0800

    Update utils.py
    fix rotate_half bug

--- modeling/attention/utils.py
+++ modeling/attention/utils.py
@@ -10,9 +10,13 @@ https://github.com/huggingface/transformers/issues/25199#issuecomment-1687720247
 https://github.com/lucidrains/rotary-embedding-torch/tree/main
 """
 
-def rotate_half(x):
+def rotate_half_old(x):
     x1, x2 = x.chunk(2, dim=-1)
     return torch.cat((-x2, x1), dim=-1)
+
+def rotate_half(x):
+    x_even, x_odd = x[:, ::2], x[:, 1::2]
+    return torch.stack([-x_odd, x_even], dim=-1).flatten(start_dim=-2, end_dim=-1)
 
 def apply_rope(q, k, cos, sin):
     q = (q * cos) + (rotate_half(q) * sin)
